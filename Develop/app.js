680c6e833e5f38bf709811907d8f533de2e9c516
littlefire <aybwhite@gmail.com>
2021-01-03 21:43:54 -0800

    deleting old code with bugs adding new . then coding

--- Develop/app.js
+++ Develop/app.js
@@ -10,6 +10,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+
 const employeeList = [];
 
 //Question prompt for position
@@ -62,14 +63,16 @@ function managerInfo () {
         },
 
     ]).then ((managerData) => {
-        const newManager = newManager(managerData.name, managerData.id, managerData.email, manager.officeNumber);
+
+        const newManager = new Manager(managerData.name, managerData.id, managerData.email, manager.officeNumber);
         employeeList.push(newManager);
         positionInfo();
     });
 }
 
 //Engineer
-const engineerInfo = () => {
+function engineerInfo() {
+
     return inquirer.prompt([
         {
             type: 'input',
@@ -79,7 +82,7 @@ const engineerInfo = () => {
         {
             type: 'input',
             name: 'id',
-            message: 'Please enter the id of this person:',
+            message: 'Please enter the ID of this person:',
         },
         {
             type: 'input',
@@ -90,12 +93,17 @@ const engineerInfo = () => {
             type: 'input',
             name: 'github',
             message: 'Please enter the github username of this person:',
-        },
-    ]);
+        }
+    ]).then((engineerData) => {
+
+        const newEngineer = new Engineer(engineerData.name, engineerData.id, engineer.email, engineerData.gitHub);
+        employeeList.push(newEngineer);
+        positionType();
+    });
 }
 
 //Intern
-const internInfo = () => {
+function internInfo () {
     return inquirer.prompt([
         {
             type: 'input',
@@ -116,63 +124,15 @@ const internInfo = () => {
             type: 'input',
             name: 'school',
             message: 'Please enter the school this person attends:',
-        },
-    ]);
+        }
+    ]).then((internData) => {
+        const newIntern = new Intern(internData.name, internData.id, internData.email, internData.school);
+        employeeList.push(newIntern);
+        positionType();
+    });
 }
 
-//Add member
-const addPosition = () => {
-    return inquirer.prompt([
-        {
-            type: 'list',
-            name: 'addPosition',
-            message: 'Do you want to add a new member?',
-            choices: ['Yes', 'No'],
-        },
-    ]);
-}
 
-//Creating HTML
-async function promptUser() {
-    console.log("============");
-    const positionType = await positionInfo();
-
-    if(positionType.position === "Manager") {
-        const managerData = await managerInfo();
-        const manager = new Manager (managerData.name, managerData.id, managerData.email, managerData.officeNumber);
-        htmlBlock.push(manager);
-
-    } else if (positionType.position === "engineer") {
-        const engineerData = await engineerInfo();
-        const engineer = new Engineer (engineerData.name, engineerData.id, engineerData.email, engineerData.github);
-        htmlBlock.push(engineer);
-
-    } else if (positionType.position === "intern") {
-        const internData = await internInfo();
-        const intern = new Intern (internData.name, internData.id, internData.email, internData.school);
-        htmlBlock.push(intern);
-    }
-
-    addNewMember();
-}
-
-//Create a function to add member
-async function addNewMember() {
-    const newMember = await addPosition();
-
-    if (newMember.addMember === "Yes") {
-        console.log("==========");
-        promptUser();
-
-    } else if (newMember.addPosition === "No") {
-        const team = render(htmlE);
-        fs.writeFileSync(outputPath, team);
-        console.log("==========");
-        console.log("Your teams HTML has been generated!")
-    }
-}
-
-promptUser();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
