f5c03b206e00dfd259b5e629644fb59e450b79eb
Amethyst White <aybwhite@gmail.com>
2021-01-04 00:34:59 -0800

    Update app.js
    Problem with file- copied from VS to gitHub

--- Develop/app.js
+++ Develop/app.js
@@ -10,149 +10,148 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 const employeeList = [];
 
-//Question prompt for position
-function positionInfo () {
-
+//Asks user which position to add
+const positionInfo = () => {
     return inquirer.prompt([
         {
             type: 'list',
-            name: 'newPosition',
-            message: 'Please choose this members position:',
-            choices: [{name: 'Engineer', value: 0}, {name:'Intern', value: 1}, {name:'No more team members', value:2}],
-        }
-    ]).then((newPositionType) => {
-        //Questions for engineer
-        if (newPositionType.newPosition === 0) {
-            engineerInfo();
-        //Questions for Intern
-        } else if (newPositionType.newPosition === 1) {
-            internInfo();
-        // Exit app
-        } else {
-            createHtmlFile();
-        }
-    });
+            name: 'role',
+            message: 'Please choose the role of this member:',
+            choices: ['Manager', 'Engineer', 'Intern'],
+        },
+    ]);
 }
 
-//Manager
-function managerInfo () {
-
+//Manager questionnaire
+const managerInfo = () => { 
     return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: 'Please enter the name of this person:',
+            message: 'Please enter the name of this member:',
         },
         {
             type: 'input',
             name: 'id',
-            message: 'Please enter the id of this person:',
+            message: 'Please enter the ID number of this member:',
         },
         {
             type: 'input',
             name: 'email',
-            message: 'Please enter the email of this person:',
+            message: 'Please enter the Email of this member:',
         },
         {
             type: 'input',
             name: 'officeNumber',
-            message: 'Please enter the office number of this person:',
+            message: 'Please enter your office number:',
         },
-
-    ]).then ((managerData) => {
-
-        const newManager = new Manager(managerData.name, managerData.id, managerData.email, manager.officeNumber);
-        employeeList.push(newManager);
-        positionInfo();
-    });
+    ]);
 }
 
-//Engineer
-function engineerInfo() {
-
+//Engineer questionnaire
+const engineerInfo = () => {
     return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: 'Please enter the name of this person:',
+            message: 'Please enter the name of this member:',
         },
         {
             type: 'input',
             name: 'id',
-            message: 'Please enter the ID of this person:',
+            message: 'Please enter the ID number of this member:',
         },
         {
             type: 'input',
             name: 'email',
-            message: 'Please enter the email of this person:',
+            message: 'Please enter the Email of this member:',
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Please enter the github username of this person:',
-        }
-    ]).then((engineerData) => {
-
-        const newEngineer = new Engineer(engineerData.name, engineerData.id, engineer.email, engineerData.gitHub);
-        employeeList.push(newEngineer);
-        positionType();
-    });
+            message: 'Please enter your github username:',
+        },
+    ]);
 }
 
-//Intern
-function internInfo () {
+//Intern questionnaire
+const internInfo = () => {
     return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: 'Please enter the name of this person:',
+            message: 'Please enter the name of this member:',
         },
         {
             type: 'input',
             name: 'id',
-            message: 'Please enter the id of this person:',
+            message: 'Please enter the ID number of this member:',
         },
         {
             type: 'input',
             name: 'email',
-            message: 'Please enter the email of this person:',
+            message: 'Please enter the Email of this member:',
         },
         {
             type: 'input',
             name: 'school',
-            message: 'Please enter the school this person attends:',
-        }
-    ]).then((internData) => {
-        const newIntern = new Intern(internData.name, internData.id, internData.email, internData.school);
-        employeeList.push(newIntern);
-        positionType();
-    });
+            message: 'Please enter your school:',
+        },
+    ]);
 }
 
+//Add more people to the team
+const addMember = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'addMember',
+            message: 'Do you want to add another new member?',
+            choices: ['Yes', 'No'],
+        },
+    ]);
+}
 
+//Creates HTML 
+async function promptUser() {
+    console.log("-----------------------");
+    const roleType = await positionInfo();
+    
+    if (roleType.role === "Manager") {
+        const managerData = await managerInfo();
+        const manager = new Manager(managerData.name, managerData.id, managerData.email, managerData.officeNumber);
+        employeeList.push(manager);
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+    } else if (roleType.role === "Engineer") {
+        const engineerData = await engineerInfo();
+        const engineer = new Engineer(engineerData.name, engineerData.id, engineerData.email, engineerData.github);
+        employeeList.push(engineer);
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+    } else if (roleType.role === "Intern") {
+        const internData = await internInfo();
+        const intern = new Intern(internData.name, internData.id, internData.email, internData.school);
+        employeeList.push(intern);
+    }
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+    addNewMember();
+}
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+// Create a function to add new member or end up to write the team html file
+async function addNewMember() {
+    const newMember = await addMember();
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+    if (newMember.addMember === "Yes") {
+        console.log("==x==");
+        promptUser();
+        
+    } else if (newMember.addMember === "No") {
+        const team = render(employeeList);
+        fs.writeFileSync(outputPath, team);
+        console.log("==x==");
+        console.log("Your teams page has been created!")
+    }
+}
+
+promptUser();
